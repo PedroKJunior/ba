2838e49468090b052bfcf1e4066b0798f8b8ba92
PedroKJunior <juniorkretikouski@gmail.com>
2025-02-19 22:13:30 -0300

    Create Logo Component

---
+++ src/components/Logo.tsx
@@ -0,0 +1,28 @@
+import { styled } from 'styled-components'
+
+const H1 = styled.h1`
+	font-family: 'Roboto', sans-serif;
+	font-size: 2.5em;
+	color: #fff;
+	background: linear-gradient(0deg, #87de1d, #b4e900);
+	background-clip: text;
+	-webkit-text-fill-color: transparent;
+	text-transform: uppercase;
+	display: flex;
+	align-items: center;
+`
+
+const Image = styled.img`
+	width: 1em;
+	height: 1em;
+	margin-right: 0.25em;
+`
+
+export function Logo() {
+	return (
+		<H1>
+			<Image src="/logo.svg" alt="B" />
+			Brain Agriculture
+		</H1>
+	)
+}
