2f2b35ba24a96e226a15860df5c274709a4027dd
PedroKJunior <juniorkretikouski@gmail.com>
2025-02-20 22:34:46 -0300

    Add Routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import { BrowserRouter as Router } from 'react-router-dom'
+
+import { GlobalStyle } from './styles/global'
+
+import { Navbar } from './components/Navbar'
+import { AppRoutes } from './routes/index.route'
 
 function App() {
-  return <></>;
+	return (
+		<>
+			<GlobalStyle />
+			<Router>
+				<Navbar />
+				<AppRoutes />
+			</Router>
+		</>
+	)
 }
 
-export default App;
+export default App
