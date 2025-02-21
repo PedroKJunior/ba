9ec210c859d91a4f040842cf53d34127b4eba4fe
PedroKJunior <juniorkretikouski@gmail.com>
2025-02-20 22:34:04 -0300

    Create View Components

---
+++ src/styles/view.tsx
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components'
+
+export const View = styled.div`
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	height: calc(100vh - 60px);
+	width: 100vw;
+	color: #f0f0f5;
+`
